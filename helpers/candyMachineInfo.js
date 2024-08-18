9e3b25ec6dd7bfa3eeea9332f465b47bae2827d5
Alexandros De Great <126134422+pichtranst123@users.noreply.github.com>
2024-08-18 22:26:10 +0700

    first release

--- helpers/candyMachineInfo.js
+++ helpers/candyMachineInfo.js
@@ -4,14 +4,14 @@ export const COLLECTION_SIZE = 100;
 
 // To remove the background of the dapp to a solid color just delete the "collectionCoverUrl" url below
 // Any hosted image can be used - jpeg, gif, png
-export const collectionCoverUrl = "/collectionCoverUrl.jpeg";
+export const collectionCoverUrl = "/logoactionx.svg";
 export const collectionBackgroundUrl =
   "https://cdn.pixabay.com/photo/2018/04/16/12/59/mountains-3324569_1280.jpg";
 
 export const mode = "dev"; // "dev" or "test" or "mainnet"
 export let NODE_URL;
 export const CONTRACT_ADDRESS =
-  "0x3c9d531fe53e7dde20b1a745922d1627943153e5f8415670ddca06773d58e426";
+  "b0b5ebcbc3cb07f5afe6bbb64892ef1241c2b33a309ee82912d9844402ceef3e";
 let FAUCET_URL;
 
 if (mode == "dev") {
